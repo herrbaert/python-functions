2597ef3fbb3fa6b9145c59a2eabcf40e95dd990a
herrbaert <herbaert@mailbox.org>
2025-03-28 10:43:35 +0100

    optimized version of greet function

---
+++ functions.py
@@ -0,0 +1,17 @@
+def greet():
+    print("Hello dear World")
+    print("End of greet function")
+
+print("Hello World")
+
+#some other code
+print("other code 1")
+
+
+print("Hello World")
+
+#some other code
+print("other code 2")
+
+
+print("Hello World")
